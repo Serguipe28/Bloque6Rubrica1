b362f098895f8f0c63dd7adda4c2c873f486518f
Serguipe28 <serguipe28@gmail.com>
2022-02-04 11:15:18 +0100

    Create project

---
+++ src/test/java/com/serguipe/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serguipe.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
